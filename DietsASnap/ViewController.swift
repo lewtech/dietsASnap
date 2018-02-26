450be213f56608dff7522fe8b0797e86db09be90
lewtech <lewtech@gmail.com>
2018-02-26 14:28:29 -0600

    Initial Commit

---
+++ DietsASnap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DietsASnap
+//
+//  Created by Lew Flauta on 2/26/18.
+//  Copyright © 2018 Lew Flauta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
